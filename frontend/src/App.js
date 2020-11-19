2aaf27e74fc55e93177f29999a8e1d0722bc2fc9
IGOR <igornesan@gmail.com>
2020-11-19 21:15:48 +0200

    show up data from local file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
+            <Route exact path="/product/:id" component={ProductScreen} />
+          </Switch>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
