f01a2eeee3ef3e51cc59cdcc1d51d4c5957f3cec
IGOR <igornesan@gmail.com>
2020-11-20 17:13:52 +0200

    add concurrently to run both:server and clien

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.get('/api/products', (req, res) => {
   res.json(products);
 });
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => (p._id = req.params.id));
+  const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await axios.get('/api/products');
+
+      setProducts(res.data);
+    };
+    fetchProducts();
+  }, []);
   return (
     <Fragment>
       <h1>Latest Product</h1>
