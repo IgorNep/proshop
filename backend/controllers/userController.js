5826048ab3fc92758aea0f1f5670f501a2647cac
IGOR <igornesan@gmail.com>
2020-11-26 22:45:19 +0200

    added register user func

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,4 +35,28 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User Already Exists');
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User Not Found');
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
