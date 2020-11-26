26f8f23d13764839a6d9d90251b6f64f5d4c2211
IGOR <igornesan@gmail.com>
2020-11-26 22:39:41 +0200

    added protect routes func

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,4 +19,20 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User Not Found');
+  }
+});
+
+export { authUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
+import { protect } from '../middleware/authMiddleware';
 
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile } from '../controllers/userController.js';
 
 router.post('/login', authUser);
-
+router.route('/profile').get(protect, getUserProfile);
 export default router;
