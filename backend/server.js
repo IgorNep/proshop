94281f368bed9306cd26c3271fca579c74066307
IGOR <igornesan@gmail.com>
2020-11-30 18:19:18 +0200

    add order route and add to server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes';
 
 const app = express();
 dotenv.config();
@@ -22,3 +23,4 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
