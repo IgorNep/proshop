308369eb8e3e39a5e4e02133a0f60ba2f2b8c6d6
IGOR <igornesan@gmail.com>
2020-11-30 18:15:59 +0200

    add order controller"

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,35 @@
+import asyncHandler from 'express-async-handler';
+import Order from '../models/orderModel';
+
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order Items');
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createOrder = await order.save();
+    res.status(201).json(createOrder);
+  }
+});
+
+export { addOrderItems };
