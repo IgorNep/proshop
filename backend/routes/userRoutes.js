cb87f5c89144e6f3c52282b626fcb8a9ae1721e2
IGOR <igornesan@gmail.com>
2020-12-10 18:01:30 +0200

    add getuserbyid and update user data - only admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,6 +9,8 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  updateUser,
+  getUserById,
 } from '../controllers/userController.js';
 
 router.route('/login').post(authUser);
@@ -17,6 +19,10 @@ router
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
 router.route('/').post(registerUser).get(protect, admin, getUsers);
-router.route('/:id').delete(protect, admin, deleteUser);
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
