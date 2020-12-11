ad7bec60af8ef2da0a12e37ca6290a56a1b2686e
IGOR <igornesan@gmail.com>
2020-12-11 18:29:52 +0200

    add delete product route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,4 +17,16 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: 'Product Removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product Not Found');
+  }
+});
+
+export { getProducts, getProductById, deleteProduct };
