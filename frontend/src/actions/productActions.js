9988a1da5ea92b846c4fb2400b9763c1439ef39a
IGOR <igornesan@gmail.com>
2020-12-11 18:33:13 +0200

    add deleteproduct reducer and constants

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -43,3 +43,21 @@ export const listProductDetails = (id) => async (dispatch) => {
     });
   }
 };
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -5,6 +5,9 @@ import {
   PRODUCT_DETAILS_FAIL,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DELETE_FAIL,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_REQUEST,
 } from '../constants/productConstants';
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -45,6 +48,27 @@ export const productDetailsReducer = (
         loading: false,
       };
     case PRODUCT_DETAILS_FAIL:
+      return {
+        error: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export const productDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_DELETE_REQUEST:
+      return {
+        loading: true,
+      };
+    case PRODUCT_DELETE_SUCCESS:
+      return {
+        success: true,
+        loading: false,
+      };
+    case PRODUCT_DELETE_FAIL:
       return {
         error: action.payload,
         loading: false,
