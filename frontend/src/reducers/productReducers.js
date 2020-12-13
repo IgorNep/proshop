aa26df0685b0ee9bb100f3daa27df93f6b670156
IGOR <igornesan@gmail.com>
2020-12-13 17:41:21 +0200

    add create review reducer and action

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -16,6 +16,10 @@ import {
   PRODUCT_UPDATE_FAIL,
   PRODUCT_UPDATE_REQUEST,
   PRODUCT_UPDATE_RESET,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAIL,
+  PRODUCT_CREATE_REVIEW_RESET,
 } from '../constants/productConstants';
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -128,6 +132,29 @@ export const productUpdateReducer = (state = { product: {} }, action) => {
         loading: false,
       };
     case PRODUCT_UPDATE_RESET:
+      return { product: {} };
+    default:
+      return state;
+  }
+};
+
+export const productReviewCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REVIEW_REQUEST:
+      return {
+        loading: true,
+      };
+    case PRODUCT_CREATE_REVIEW_SUCCESS:
+      return {
+        success: true,
+        loading: false,
+      };
+    case PRODUCT_CREATE_REVIEW_FAIL:
+      return {
+        error: action.payload,
+        loading: false,
+      };
+    case PRODUCT_CREATE_REVIEW_RESET:
       return { product: {} };
     default:
       return state;
