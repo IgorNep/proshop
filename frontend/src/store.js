c78622a3da376b852eac952f5abb305c9f4ae6b6
IGOR <igornesan@gmail.com>
2020-12-13 15:35:56 +0200

    add order deliver reducer and constants

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -24,6 +24,7 @@ import {
   orderPayReducer,
   orderListMyReducer,
   orderListReducer,
+  orderDeliverReducer,
 } from './reducers/orderReducers';
 
 const reducer = combineReducers({
@@ -45,6 +46,7 @@ const reducer = combineReducers({
   orderPay: orderPayReducer,
   orderListMy: orderListMyReducer,
   orderList: orderListReducer,
+  orderDeliver: orderDeliverReducer,
 });
 
 const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
