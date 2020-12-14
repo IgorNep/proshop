915a9b552ea0f0b2c11f91a450868b716b29a4ae
IGOR <igornesan@gmail.com>
2020-12-14 16:13:11 +0200

    add backend functionality for search

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,10 @@ import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
+  const keyword = req.query.keyword
+    ? { name: { $regex: req.query.keyword, $options: 'i' } }
+    : {};
+  const products = await Product.find({ ...keyword });
   res.json(products);
 });
 
